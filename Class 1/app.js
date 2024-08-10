f42b631abba412a6df06fa4de31ace4da5862cc9
unknown <you@example.com>
2024-08-10 13:44:24 +0100

    Made the page responsiv 1

--- Class 1/app.js
+++
@@ -1,5 +0,0 @@
-// const aboutButton = document.getElementById("about_button")
-// aboutButton.addEventListener("click", (e) => {
-//     e.preventDefault()
-//     window.location.href = "/about.html"
-// })
